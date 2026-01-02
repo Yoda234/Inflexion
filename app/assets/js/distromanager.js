2ae90a7ceba4b363098235e6fa211fce71f86e79
Yodish <veillette2056@gmail.com>
2026-01-02 15:34:47 -0500

    Caca

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://panel.infllexionhost.eu:25568/launcher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://launcher.invergencenetwork.eu/launcher/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
